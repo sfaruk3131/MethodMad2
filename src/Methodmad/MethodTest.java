2f288b6cd14ef80932e2043ee205bc5cc8c5a903
sfaruk3131 <sfaruk3131@bths.edu>
2019-03-30 20:23:48 -0400

    Initial commit

---
+++ src/Methodmad/MethodTest.java
@@ -0,0 +1,13 @@
+package Methodmad;
+
+import static Methodmad.Methods.*;
+
+public class MethodTest {
+    public static void main(String[] args)
+    {
+        System.out.println(sumUpto(6));
+        System.out.println(multiplicationTable(5,12));
+        System.out.println(dateStr("12/27/2001"));
+        System.out.println(isPalindrome("foo"));
+    }
+}
---
+++ src/Methodmad/Methods.java
@@ -0,0 +1,36 @@
+package Methodmad;
+
+public class Methods {
+    public static int sumUpto(int Integer)
+    {
+        int sum = 0;
+        for(int i = 0; i <= Integer;i++)
+        {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    public static String multiplicationTable(int base, int range)
+    {
+        String end = "the end";
+        for(int i = 0; i <= range;i++)
+        {
+           int product = base * i;
+           System.out.println(product);
+        }
+        return end;
+    }
+    public static String dateStr(String mdy)
+    {
+        String partA = mdy.substring(0,2);
+        String partB = mdy.substring(3,5);
+        String partC = mdy.substring(6);
+        return partB + "-" + partA + "-" + partC;
+    }
+    public static boolean isPalindrome(String input)
+    {
+        String rev = "";
+        
+    }
+
+}
